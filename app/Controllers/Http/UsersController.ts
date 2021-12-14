a08a3d8585fc4ed0dd3c89059f071ef75a7259be
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2021-12-14 10:56:41 -0500

    Comment validation for view users

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -14,7 +14,7 @@ import { IUser } from "../../Utils/interfaces/user";
 import {
   base64encode,
   getPermitsAndRoles,
-  hasPermit,
+  // hasPermit,
   messageError,
   sum,
   validatePagination,
@@ -46,23 +46,23 @@ export default class UsersController {
 
     let detailsUsers: DetailsUser[], detailsUser;
 
-    const permitsAnsRolesPetitioner = await getPermitsAndRoles(
-      request,
-      response,
-      payloadToken.id
-    );
+    // const permitsAnsRolesPetitioner = await getPermitsAndRoles(
+    //   request,
+    //   response,
+    //   payloadToken.id
+    // );
 
-    const boolHasPermit = hasPermit(
-      permitsAnsRolesPetitioner.permits,
-      "detalles_Usuarios"
-    );
+    // const boolHasPermit = hasPermit(
+    //   permitsAnsRolesPetitioner.permits,
+    //   "detalles_Usuarios"
+    // );
 
-    if (!boolHasPermit) {
-      responseData["message"] =
-        "No posee el permiso para ver el detalle del usuario.";
-      responseData["error"] = true;
-      return response.status(400).json(responseData);
-    }
+    // if (!boolHasPermit) {
+    //   responseData["message"] =
+    //     "No posee el permiso para ver el detalle del usuario.";
+    //   responseData["error"] = true;
+    //   return response.status(400).json(responseData);
+    // }
 
     const { roles, permits } = await getPermitsAndRoles(request, response, id);
 
